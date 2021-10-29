56e767c697d886b8f2e1b697e99812665f34391b
KMD98 <59901921+KMD98@users.noreply.github.com>
2021-10-29 13:20:23 -0500

    Update I2CMasterSTM.ino

--- I2CMasterSTM/I2CMasterSTM.ino
+++ I2CMasterSTM/I2CMasterSTM.ino
@@ -1,17 +1,23 @@
 #include <Wire.h>
+struct 3Dcoor{
+  //24 bytes in total to be send
+  int left_lat;
+  int right_lat;
+  int left_lon;
+  int right_lon;
+  int left_height;
+  int right_height;
+}
+//Initialize instance for drone data
+struct 3Dcoor drone_coor;
 
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
+  Serial.begin(19200);  // start serial for output
 }
 
 void loop() {
-  Wire.requestFrom(8, 6);    // request 6 bytes from slave device #8
-
-  while (Wire.available()) { // slave may send less than requested
-    char c = Wire.read(); // receive a byte as character
-    Serial.print(c);         // print the character
-  }
-
-  delay(500);
+  Wire.requestFrom(8, 24);    // request 6 bytes from slave device #8
+  Wire.readBytes((byte*)&drone_coor, sizeof(drone_coor));
+  delay(1000);
 }

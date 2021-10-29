fcfd5b509f27da7b61c21b89408dd4353d9067ca
KMD98 <59901921+KMD98@users.noreply.github.com>
2021-10-29 13:22:55 -0500

    Update I2CMasterSTM.ino

--- I2CMasterSTM/I2CMasterSTM.ino
+++ I2CMasterSTM/I2CMasterSTM.ino
@@ -10,14 +10,16 @@ struct 3Dcoor{
 }
 //Initialize instance for drone data
 struct 3Dcoor drone_coor;
-
+struct 3Dcoor MGCP_coor;
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(19200);  // start serial for output
 }
 
 void loop() {
-  Wire.requestFrom(8, 24);    // request 6 bytes from slave device #8
+  Wire.requestFrom(8, 24);    // request 24 bytes from slave device #8 Drone 3d coor
   Wire.readBytes((byte*)&drone_coor, sizeof(drone_coor));
+  Wire.requestFrom(9,24); //Request 24 bytes from salve device #9 MGCP 3d coor
+  Wire.readBytes((byte*)&MGCP_coor, sizeof(MGCP_coor));
   delay(1000);
 }

7646a2b8fbeee3bd090c54bf3c4997c75c962856
KMD98 <59901921+KMD98@users.noreply.github.com>
2021-11-02 11:43:43 -0500

    Update I2CMasterSTM.ino

--- I2CMasterSTM/I2CMasterSTM.ino
+++ I2CMasterSTM/I2CMasterSTM.ino
@@ -1,5 +1,5 @@
 #include <Wire.h>
-struct 3Dcoor{
+struct coor{
   //24 bytes in total to be send
   int left_lat;
   int right_lat;
@@ -9,8 +9,8 @@ struct 3Dcoor{
   int right_height;
 }
 //Initialize instance for drone data
-struct 3Dcoor drone_coor;
-struct 3Dcoor MGCP_coor;
+struct coor drone_coor;
+struct coor MGCP_coor;
 void setup() {
   Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(19200);  // start serial for output
@@ -19,7 +19,10 @@ void setup() {
 void loop() {
   Wire.requestFrom(8, 24);    // request 24 bytes from slave device #8 Drone 3d coor
   Wire.readBytes((byte*)&drone_coor, sizeof(drone_coor));
-  Wire.requestFrom(9,24); //Request 24 bytes from salve device #9 MGCP 3d coor
-  Wire.readBytes((byte*)&MGCP_coor, sizeof(MGCP_coor));
+  Serial.print(drone_coor.left_lat);Serial.print(".");Serial.println(drone_coor.right_lat);
+  Serial.print(drone_coor.left_lon);Serial.print(".");Serial.println(drone_coor.right_lon);
+  Serial.print(drone_coor.left_height);Serial.print(".");Serial.println(drone_coor.right_height);
+  /*Wire.requestFrom(9,24); //Request 24 bytes from salve device #9 MGCP 3d coor
+  Wire.readBytes((byte*)&MGCP_coor, sizeof(MGCP_coor));*/
   delay(1000);
 }

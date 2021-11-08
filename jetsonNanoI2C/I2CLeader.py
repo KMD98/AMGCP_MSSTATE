d7c523ee5020bfc83d24678b7dc48f03a4ed7da1
KMD98 <59901921+KMD98@users.noreply.github.com>
2021-11-08 14:33:39 -0600

    Update I2CLeader.py

--- jetsonNanoI2C/I2CLeader.py
+++ jetsonNanoI2C/I2CLeader.py
@@ -7,21 +7,10 @@ bus = SMBus(1)
     
 #I2C subroutines for master and send back data from each slave
 def readingI2Cbus(addr):
-    #grab drone coordinates
-    data = []
-    #receives the length in two bytes and then convert that into an int
-    high = bus.read_byte(addr)
-    low = bus.read_byte(addr)
-    length = (high<<8) + low
-    for i in range (0,length): #need to be dynamic
-        data.append(chr(bus.read_byte(addr)))
-    data.pop() #remove last element delimiter
-    data.remove('!') #remove all limiter in the beginning
-    return ''.join(data)
-
-def coordinateParse(coordinates):
-    temp_list = coordinates.split(',')
-    return float(temp_list[0]), float(temp_list[1]),float(temp_list[2]), float(temp_list[3]) #lat, lon, height above ellipsoid, hmsl
+    #grab coordinates
+    data = bus.read_i2c_block_data(addr,0,19);
+    print(data)
+    #return ''.join(data)
 
 #def writeRTM(data): #send to real time monitoring sender uncomment when ready
     #for i in range (0,len(data)):
@@ -29,11 +18,10 @@ def coordinateParse(coordinates):
 
 if __name__ == '__main__':
     while True:
-        reading = bus.read_i2c_block_data(addr_droneCoor, 0, 16)
-        print(reading)
-        #testing lat value being passed
+        readingI2Cbus(addr_droneCoor)
+        '''#testing lat value being passed
         latleft = (reading[0]<<8) + reading[1]
         print(latleft)
         latright = (reading[2]<<8) + reading[3]
-        print(latright)
+        print(latright)'''
         time.sleep(1.0)

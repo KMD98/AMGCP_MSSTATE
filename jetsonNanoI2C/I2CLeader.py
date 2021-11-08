aa361f2c1f00bea37de5b1204679bce4e57528a3
KMD98 <59901921+KMD98@users.noreply.github.com>
2021-11-08 15:42:29 -0600

    Update I2CLeader.py

--- jetsonNanoI2C/I2CLeader.py
+++ jetsonNanoI2C/I2CLeader.py
@@ -8,11 +8,19 @@ bus = SMBus(1)
 #I2C subroutines for master and send back data from each slave
 def readingI2Cbus(addr):
     #grab coordinates
-    data = bus.read_i2c_block_data(addr,0,19);
-    print(data)
-    print(type(data[0]))
-    #return ''.join(data)
+    temp = bus.read_i2c_block_data(addr,0,19)
+    bytetoInt(temp)
+    #return ''.join(temp)
 
+def bytetoInt(temp):
+    data = []
+    i = 0
+    while i < 17:
+        data[i] = temp[i]<<8 + temp[i+1]
+        i = i + 2
+    print(data)
+    #return char array
+    
 #def writeRTM(data): #send to real time monitoring sender uncomment when ready
     #for i in range (0,len(data)):
         #bus.write_byte(0x06,data[i]) #address of real time monitor sender
@@ -20,9 +28,4 @@ def readingI2Cbus(addr):
 if __name__ == '__main__':
     while True:
         readingI2Cbus(addr_droneCoor)
-        '''#testing lat value being passed
-        latleft = (reading[0]<<8) + reading[1]
-        print(latleft)
-        latright = (reading[2]<<8) + reading[3]
-        print(latright)'''
         time.sleep(1.0)

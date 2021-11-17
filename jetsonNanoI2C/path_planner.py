675e4623008925567fd6a6ab328b8de37e09014a
KMD98 <59901921+KMD98@users.noreply.github.com>
2021-11-17 13:41:30 -0600

    Update path_planner.py

--- jetsonNanoI2C/path_planner.py
+++ jetsonNanoI2C/path_planner.py
@@ -2,25 +2,39 @@
 import rospy
 from ros_essentials_cpp.msg import RTK
 
-def odometry_reading(RTK_data):
-    '''rospy.loginfo("new RTK drone data received: (%.7f, %.7f, %.2f)", 
-        RTK_data.drone_lat,RTK_data.drone_lon,RTK_data.drone_height)
-    rospy.loginfo("new RTK MGCP data received (%.7f, %.7f, %.3f, %.2f)", 
-        RTK_data.MGCP_lat,RTK_data.MGCP_lon,RTK_data.MGCP_height,RTK_data.MGCP_heading)'''
-    rospy.loginfo(RTK_data)
-def navigator():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('navigator', anonymous=True)
-
-    rospy.Subscriber("RTK_odometry_topic", RTK, odometry_reading)
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-
+class NodeSubscriber:
+    def __init__(self):
+        #initialize node
+        # In ROS, nodes are uniquely named. If two nodes with the same
+        # node are launched, the previous one is kicked off. The
+        # anonymous=True flag means that rospy will choose a unique
+        # name for our 'listener' node so that multiple listeners can
+        # run simultaneously.
+        rospy.init_node('navigator', anonymous=True)
+        self.node_name = rospy.get_name()
+        rospy.loginfo("Started node %s" % self.nodename)
+        self.odometry_data = []
+        #this node subribes to the RTK_odometry_topic which the odometry publisher publishes to
+        rospy.Subscriber("RTK_odometry_topic", RTK, odometry_callback)
+        
+    
+    def odometry_callback(self, RTK_data):
+        self.odometry_data.clear()
+        #we dont really care for height so we can skip that
+        self.odometry_data.append(float(RTK_data.drone_lat))
+        self.odometry_data.append(float(RTK_data.drone_lon))
+        self.odometry_data.append(float(RTK_data.MGCP_lat))
+        self.odometry_data.append(float(RTK_data.MGCP_lon))
+        self.odometry_data.append(float(RTK_data.MGCP_heading))
+    
+    def spin(self):
+        while not rospy.is_shutdown():
+            rospy.loginfo(self.odometry_data)
+            
 if __name__ == '__main__':
-    navigator()
+    try:
+        navigator = NodeSubscriber()
+        navigator.spin()
+    except rospy.ROSInterruptException:
+        pass
+        

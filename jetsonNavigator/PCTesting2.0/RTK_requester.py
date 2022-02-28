e960a03bf11622c95719d58b0ba3c6ac35a0d1a8
KMD98 <59901921+KMD98@users.noreply.github.com>
2022-02-28 10:45:50 -0600

    Update RTK_requester.py

--- jetsonNavigator/PCTesting2.0/RTK_requester.py
+++ jetsonNavigator/PCTesting2.0/RTK_requester.py
@@ -119,9 +119,9 @@ def odometryPub():
         amgcp_data.amgcp_height = 40.129
         amgcp_data.speed2D = 1000
         amgcp_data.bearing = 250.30
-        rospy.loginfo("I published: ")
+        '''rospy.loginfo("I published: ")
         rospy.loginfo(drone_data)
-        rospy.loginfo(amgcp_data)
+        rospy.loginfo(amgcp_data)'''
         uav_pub.publish(drone_data)
         ugs_pub.publish(amgcp_data)
         rate.sleep()

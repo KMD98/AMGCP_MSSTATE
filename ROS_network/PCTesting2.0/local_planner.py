daa927ddedd61ba42bf478dfe9f671e1ab7bb042
KMD98 <59901921+KMD98@users.noreply.github.com>
2022-03-10 15:02:38 -0600

    Update local_planner.py

--- ROS_network/PCTesting2.0/local_planner.py
+++ ROS_network/PCTesting2.0/local_planner.py
@@ -90,10 +90,7 @@ class localPlanner:
 
         #Initialize the rotation_matrix at inital starting position. The robot should not be moving at this initiation and RTK should be available and checked as indicated by operator.
         #Note that as long as the operator FLIPPED the RTK switch when RTK IS available, the rotation matrix will always be correct.
-        if abs(self.displacement_vect[4]) <= 90.0):
-            temp_theta = 90.0 - self.displacement_vect[4]
-        elif abs(self.displacement_vect[4] > 90.0):
-            temp_theta = self.displacement_vect[4] - 90.0 
+        temp_theta = self.displacement_vect[4] - 90.0
         self.rotation_matrix[[0,1],[0,1]] = cos(radians(temp_theta))
         self.rotation_matrix[0][1] = -1 * sin(radians(temp_theta))
         self.rotation_matrix[1][0] = sin(radians(temp_theta))

71a25c9d24fd4d11786c787b83592710f3b9e010
KMD98 <59901921+KMD98@users.noreply.github.com>
2022-05-02 11:44:07 -0500

    Update motor_driver.py

--- ROS_network/PCTesting2.0/motor_driver.py
+++ ROS_network/PCTesting2.0/motor_driver.py
@@ -18,17 +18,13 @@ class MotorDriver:
 		rospy.Subscriber("/motors/manual_speeds",motor_rpm,self.manual_callback)
 		#Declare the topic that it is going to publish to, in this case the topic have real encoder readings
 		self.pub = rospy.Publisher('/motors/feedback_rpm',encoder_odometry,queue_size=10)
-		self.rate = rospy.Rate(10) #set to 10Hz
+		self.rate = rospy.Rate(20) #set to 20Hz
 
 	def setspeed_callback(self,autonomous_data):
 		self.speed_data[0] = autonomous_data.driver_side
 		self.speed_data[1] = autonomous_data.driver_dir
 		self.speed_data[2] = autonomous_data.passenger_side
 		self.speed_data[3] = autonomous_data.passenger_dir
-		if np.sum(self.speed_data) != np.sum(self.previous_speeds): #if not the same then send new desired speed commands
-			self.bus.write_i2c_block_data(self.addr,1,self.speed_data.tolist())
-			for i in range(0,4):
-				self.previous_speeds[i] = self.speed_data[i] #initialize "previous array" with current values for next comparison
 
 	def manual_callback(self,manual_data):
 		self.speed_data[0] = manual_data.driver_side
@@ -57,10 +53,12 @@ class MotorDriver:
 		self.pub(actual_rpm)
 
 	def spin(self):
-		rospy.spin()
-		'''while not rospy.is_shutdown():
-			self.rpm_publish()
-			self.rate.sleep()'''
+		while not rospy.is_shutdown():
+			if not np.array_equal(self.speed_data,self.previous_speeds): #if not the same then send new desired speed commands
+				self.bus.write_i2c_block_data(self.addr,1,self.speed_data.tolist())
+				for i in range(0,4):
+					self.previous_speeds[i] = self.speed_data[i] #initialize "previous array" with current values for next comparison			
+			self.rate.sleep()
 
 if __name__ == '__main__':
 	try:

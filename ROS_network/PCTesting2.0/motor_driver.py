db11730cd84e23465b0a197635fe712cef5c29b3
KMD98 <59901921+KMD98@users.noreply.github.com>
2022-05-02 14:43:30 -0500

    Update motor_driver.py
    changed i2c sending to low level controller to a function that is called by each callback (manual or autonomous)

--- ROS_network/PCTesting2.0/motor_driver.py
+++ ROS_network/PCTesting2.0/motor_driver.py
@@ -14,10 +14,10 @@ class MotorDriver:
 		self.node_name = rospy.get_name()
 		rospy.loginfo("Started node %s" % self.node_name)
 		#Declare the topic that it is subscribed to, in this case the desired motor speeds
-		rospy.Subscriber("/motors/autonomous_speeds",motor_rpm,self.setspeed_callback)
-		rospy.Subscriber("/motors/manual_speeds",motor_rpm,self.manual_callback)
+		rospy.Subscriber("/control/autonomous_speeds",motor_rpm,self.setspeed_callback)
+		rospy.Subscriber("/control/manual_speeds",motor_rpm,self.manual_callback)
 		#Declare the topic that it is going to publish to, in this case the topic have real encoder readings
-		self.pub = rospy.Publisher('/motors/feedback_rpm',encoder_odometry,queue_size=10)
+		self.pub = rospy.Publisher('/control/feedback_rpm',encoder_odometry,queue_size=10)
 		self.rate = rospy.Rate(20) #set to 20Hz
 
 	def setspeed_callback(self,autonomous_data):
@@ -25,13 +25,20 @@ class MotorDriver:
 		self.speed_data[1] = autonomous_data.driver_dir
 		self.speed_data[2] = autonomous_data.passenger_side
 		self.speed_data[3] = autonomous_data.passenger_dir
+		self.send_commands()
 
 	def manual_callback(self,manual_data):
 		self.speed_data[0] = manual_data.driver_side
 		self.speed_data[1] = manual_data.driver_dir
 		self.speed_data[2] = manual_data.passenger_side
 		self.speed_data[3] = manual_data.passenger_dir
-				
+		self.send_commands()
+	
+	def send_commands(self):
+		if not np.array_equal(self.speed_data,self.previous_speeds): #if not the same then send new desired speed commands
+			self.bus.write_i2c_block_data(self.addr,1,self.speed_data.tolist())
+			for i in range(0,4):
+				self.previous_speeds[i] = self.speed_data[i] #initialize "previous array" with current values for next comparison
 	def get_rpm(self):
 		return self.bus.read_i2c_block_data(self.addr,0,8)
 	
@@ -49,12 +56,9 @@ class MotorDriver:
 		self.pub.publish(actual_rpm)
 
 	def spin(self):
-		while not rospy.is_shutdown():
-			if not np.array_equal(self.speed_data,self.previous_speeds): #if not the same then send new desired speed commands
-				self.bus.write_i2c_block_data(self.addr,1,self.speed_data.tolist())
-				for i in range(0,4):
-					self.previous_speeds[i] = self.speed_data[i] #initialize "previous array" with current values for next comparison			
-			self.rate.sleep()
+		self.spin()
+		'''while not rospy.is_shutdown():			
+			self.rate.sleep()'''
 
 if __name__ == '__main__':
 	try:

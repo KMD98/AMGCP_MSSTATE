5e88d465bc2b4af06aee790a45ba2ae92ea6503b
KMD98 <59901921+KMD98@users.noreply.github.com>
2022-05-02 14:21:44 -0500

    Update local_planner.py

--- ROS_network/PCTesting2.0/local_planner.py
+++ ROS_network/PCTesting2.0/local_planner.py
@@ -105,20 +105,15 @@ class localPlanner:
     def move_motors(self,speed1,dir1,speed2,dir2):
         #send to the desired motor speed topic
         motor_speeds = motor_rpm()
-        motor_speeds.passenger_side = byte(speed1)
-        motor_speeds.passenger_dir = byte(dir1)
-        motor_speeds.driver_side = byte(speed2)
-        motor_speeds.driver_dir = byte(dir2)
+        motor_speeds.passenger_side = speed1
+        motor_speeds.passenger_dir = dir1
+        motor_speeds.driver_side = speed2
+        motor_speeds.driver_dir = dir2
         self.pub.publish(motor_speeds)
 
     def spin(self):
-        #write 0,1,0,1 to motor driver
-        initial_speed = motor_rpm()
-        initial_speed.driver_side = byte(0)
-        initial_speed.driver_dir = byte(1)
-        initial_speed.passenger_side = byte(0)
-        initial_speed.passenger_dir = byte(1)
-        self.pub.publish(initial_speed)
+        #write 0,1,0,1 to motor driver to ensure that the robot is not moving at the start.
+        self.move_motors(0,1,0,1)
 
         # If RTK is not available because the operator has not indicated so, stuck in a loop until reliable and RTK heading and position is available.
         # Note that RTK should not be switched on until the robot is in the operating field and grabbed its first RTK heading and position reading. Else, RTK should be off at all , even in manual mode.
